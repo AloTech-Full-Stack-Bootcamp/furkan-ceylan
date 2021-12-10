2353932125b0ca7756f39c4b9df8f1bde4254496
furkan ceylan <ceylan.furkan@outlook.com>
2021-12-10 23:19:48 +0300

    Add week-7 homework

---
+++ week-7/gen_dec.py
@@ -0,0 +1,25 @@
+import random
+
+def random_num_gen(n, l):
+    nums = []
+    while len(nums) < n:
+        num = random.randint(10**(l-1), 10**(l)-1)
+        if num not in nums:
+            nums.append(num)
+            yield num
+
+print(list(random_number_generator(7, 6)))
+
+def my_awesome_decorator(fun):
+    def wrapped(*args):
+        return not fun(*[i+1 for i in args])
+    return wrapped
+
+@my_awesome_decorator
+def mod_batch(*nums):
+    for i in nums:
+        if i % 3 != 0:
+            return False
+    return True
+
+print(mod_batch(2, 5, 8))
